10d3cff07ff442f5fbba1a554b1ff76f8c78a887
hayzamjs <hayzam@gmail.com>
2019-04-09 12:38:16 -0700

    Change cmake and make parameters a bit
    All hacks for the time being just need to check if this is working will fix the rest of the issues later, use release-all -j[CORES]

--- src/crypto/yespower.c
+++ src/crypto/yespower.c
@@ -31,16 +31,14 @@
 #include "yespower.h"
 #include "sysendian.h"
 
-static const yespower_params_t v1 = {YESPOWER_0_5, 4096, 16, "Client Key", 10};
-
 static const yespower_params_t v2 = {YESPOWER_1_0, 4096, 16, NULL, 0};
 
-int yespower_hash(const char *input, char *output)
+int yespower_hash(const unsigned char *input, char *output)
 {
     uint32_t time = le32dec(&input[68]);
     if (time > 1553904000) {
         return yespower_tls(input, 80, &v2, (yespower_binary_t *) output);
     } else {
-        return yespower_tls(input, 80, &v1, (yespower_binary_t *) output);
+        return yespower_tls(input, 80, &v2, (yespower_binary_t *) output);
     }
 }
